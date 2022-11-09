b7d110a80accc7c92f277fdb59b3942076b199d7
AndresR25 <andresr25.07@gmail.com>
2022-11-09 17:52:08 +0100

    PracticaNo3

---
+++ PracticaProgramadaNo3/Controllers/AlumnoController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PracticaProgramadaNo3.Models;
+
+namespace PracticaProgramadaNo3.Controllers
+{
+    public class AlumnoController : Controller
+    {
+        
+        // GET: Alumno
+        public ActionResult Index(PracticaProgramadaNo3.Models.ViewModel.AlumnoModelo model)
+        {
+            if(model == null)
+            {
+                model=new Models.ViewModel.AlumnoModelo();
+            }
+            return View();
+        }
+
+        public ActionResult Guardar(PracticaProgramadaNo3.Models.ViewModel.AlumnoModelo model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction("Index", model);
+            }
+
+            Alumno alumno = new Alumno();
+            alumno.carnet = model.carnet;
+            alumno.nombre= model.nombre;
+            alumno.apellido = model.apellido;
+            alumno.genero = model.genero;
+            alumno.email = model.email;
+            alumno.telefono=model.telefono;
+            alumno.FechaNacimiento = model.FechaNacimiento;
+            PracticaNo3Entities db = new PracticaNo3Entities();
+            db.Alumno.Add(alumno);
+            db.SaveChanges();
+
+            return View("Exito",model);
+        }
+
+        public ActionResult Exito()
+        {
+            return View();
+        }
+    }
+}
---
+++ PracticaProgramadaNo3/Models/ViewModel/AlumnoModelo.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PracticaProgramadaNo3.Models.ViewModel
+{
+    public class AlumnoModelo
+    {
+        [Display(Name = "carnet")]
+        [Required(ErrorMessage = "Campo carnet es obligatorio")]
+        [RegularExpression("^[a-zA-Z]+$", ErrorMessage = "Solo letras de la A a la Z")]
+        [MaxLength(6, ErrorMessage = "El carnet tiene que tener maximo 6 caracteres")]
+        [CarnetExiste]
+        public string carnet { get; set; }
+
+        [Display(Name = "nombre")]
+        [Required(ErrorMessage = "Nombre es obligatorio")]
+        [MaxLength(30, ErrorMessage = "El nombre tiene que tener maximo 30 caracteres")]
+        public string nombre { get; set; }
+
+        [Display(Name = "apellido")]
+        [Required(ErrorMessage = "Apellido es obligatorio")]
+        [MaxLength(30, ErrorMessage = "El apellido tiene que tener maximo 30 caracteres")]
+        public string apellido { get; set; }
+
+        [Display(Name = "genero")]
+        [Required(ErrorMessage = "Genero es obligatorio")]
+        [MaxLength(1, ErrorMessage = "El genero tiene que tener maximo 1 caracter")]
+        public string genero { get; set; }
+
+        [EmailAddress(ErrorMessage = "Direccion de correo Electronico")]
+        [Display(Name = "email")]
+        [Required(ErrorMessage = "Email es obligatorio")]
+        [MaxLength(50, ErrorMessage = "El email tiene que tener maximo 50 caracteres")]
+        public string email { get; set; }
+
+        
+        [Display(Name = "telefono")]
+        [Required(ErrorMessage = "Telefono es obligatorio")]
+        [Phone(ErrorMessage ="Este campo es un telefono")]
+        [MaxLength(40, ErrorMessage = "El telefono tiene que tener maximo 40 caracteres")]
+        public string telefono { get; set; }
+
+        [Display(Name = "FechaNacimiento")]
+        [Required(ErrorMessage = "Fecha de nacimiento es obligatorio")]
+        [DataType(DataType.Date)]
+        public DateTime FechaNacimiento { get; set; }
+
+        public class CarnetExiste : ValidationAttribute
+        {
+            protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+            {
+                using(PracticaNo3Entities db= new PracticaNo3Entities())
+                {
+                    string codigo = (string)value;
+                    if (db.Alumno.Where(d => d.carnet == codigo).Count() > 0)
+                        return new ValidationResult("Numero de carnet ya existe");
+                }
+                return ValidationResult.Success;
+            }
+        }
+    }
+}
